d2d237d51a9f67ad05f7b290c6c6496c4eff1162
Ibo Ibelings <iboibelings@gmail.com>
2019-01-21 17:38:29 +0100

    deleted run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-#processing-java --sketch=/Users/iboibelings/Desktop/Artez/Jaar_4/projectmaand/max/audioDecoder --force --present
-
-
-processing-java --sketch=/Users/iboibelings/Desktop/Artez/Jaar_4/projectmaand/max/audioDecoder  --present
-
-
