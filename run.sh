4ec8759c5c4c2484698b0671a6217413d36f0dfe
Ibo Ibelings <iboibelings@gmail.com>
2019-01-21 17:26:49 +0100

    test

---
+++ run.sh
@@ -0,0 +1,6 @@
+#processing-java --sketch=/Users/iboibelings/Desktop/Artez/Jaar_4/projectmaand/max/audioDecoder --force --present
+
+
+processing-java --sketch=/Users/iboibelings/Desktop/Artez/Jaar_4/projectmaand/max/audioDecoder  --present
+
+
